9c9bb264d79cbde0e81beb0ef7432eef59e47973
straw-sparrow <mugiwara.suzume@gmail.com>
2022-05-07 19:40:16 +0900

    format

--- src/games/Reversi.java
+++ src/games/Reversi.java
@@ -5,73 +5,73 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.*;
- 
+
 public class Reversi extends JPanel {
-	
-	/**　マス目の大きさ　*/
+
+	/** マス目の大きさ */
 	private final static int cellSize = 100;
-	/**　マス目と駒の比率　*/
+	/** マス目と駒の比率 */
 	private final static int masuyohaku = (int) (cellSize * 0.1);
-	/**　上部余白　*/
+	/** 上部余白 */
 	private final static int upBlank = cellSize;
-	/**　左部余白　*/
+	/** 左部余白 */
 	private final static int leftBlank = cellSize;
-	
-	/**　
+
+	/**
 	 * 白黒ターン<br>
-	 * true:白 / false:黒　
+	 * true:白 / false:黒
 	 */
 	private boolean bwTurn = true;
-	
-	/**　8方向走査y　*/
-    private int dr[] = {-1,-1,-1, 0, 0, 1, 1, 1};
-	/**　8方向走査x　*/
-    private int dc[] = {-1, 0, 1,-1, 1,-1, 0, 1};
-	
-	
-	private String[][] board = {
-			{" ","1","2","3","4","5","6","7","8"," "},
-			{"1","・","・","・","・","・","・","・","・"," "},
-			{"2","・","・","・","・","・","・","・","・"," "},
-			{"3","・","・","・","・","・","・","・","・"," "},
-			{"4","・","・","・","○","●","・","・","・"," "},
-			{"5","・","・","・","●","○","・","・","・"," "},
-			{"6","・","・","・","・","・","・","・","・"," "},
-			{"7","・","・","・","・","・","・","・","・"," "},
-			{"8","・","・","・","・","・","・","・","・"," "},
-			{" "," "," "," "," "," "," "," "," "," "}
-	};
+
+	/** 8方向走査y */
+	private int dr[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	/** 8方向走査x */
+	private int dc[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	private String[][] board = { 
+			{ " ", "1", "2", "3", "4", "5", "6", "7", "8", " " },
+			{ "1", "・", "・", "・", "・", "・", "・", "・", "・", " " }, 
+			{ "2", "・", "・", "・", "・", "・", "・", "・", "・", " " },
+			{ "3", "・", "・", "・", "・", "・", "・", "・", "・", " " }, 
+			{ "4", "・", "・", "・", "○", "●", "・", "・", "・", " " },
+			{ "5", "・", "・", "・", "●", "○", "・", "・", "・", " " }, 
+			{ "6", "・", "・", "・", "・", "・", "・", "・", "・", " " },
+			{ "7", "・", "・", "・", "・", "・", "・", "・", "・", " " }, 
+			{ "8", "・", "・", "・", "・", "・", "・", "・", "・", " " },
+			{ " ", " ", " ", " ", " ", " ", " ", " ", " ", " " }
+			};
+
 	/**
 	 * コンストラクタ
-	 */   
+	 */
 	public Reversi() {
-        setPreferredSize(new Dimension(cellSize * 10, cellSize * 10));
-        addMouseListener(new MouseProc());
-    }
-    
+		setPreferredSize(new Dimension(cellSize * 10, cellSize * 10));
+		addMouseListener(new MouseProc());
+	}
+
 	/**
 	 * 画面描画
 	 */
-    public void paintComponent(Graphics g) {
-    	// 盤面描画
-    	initBoardDraw(g);
+	public void paintComponent(Graphics g) {
+		// 盤面描画
+		initBoardDraw(g);
 		// 駒描画
-    	pieceDraw(g);
-    	
-    }
+		pieceDraw(g);
+
+	}
 
 	/**
 	 * 盤面描画
 	 */
-    private void initBoardDraw(Graphics g) {
-        // 背景
-        g.setColor(Color.black);
-        g.fillRect(0, 0, cellSize * 10, cellSize * 10);
-        g.setColor(new Color(0, 170, 0));
-        g.fillRect(cellSize, cellSize, cellSize * 8, cellSize * 8);
+	private void initBoardDraw(Graphics g) {
+		// 背景
+		g.setColor(Color.black);
+		g.fillRect(0, 0, cellSize * 10, cellSize * 10);
+		g.setColor(new Color(0, 170, 0));
+		g.fillRect(cellSize, cellSize, cellSize * 8, cellSize * 8);
 
-        // 横線
-        g.setColor(Color.black);
+		// 横線
+		g.setColor(Color.black);
 		for (int i = 0; i < 9; i++) {
 			g.drawLine(cellSize, cellSize * (i + 1), cellSize * 9, cellSize * (i + 1));
 		}
@@ -79,7 +79,7 @@ public class Reversi extends JPanel {
 		for (int i = 0; i < 9; i++) {
 			g.drawLine(cellSize * (i + 1), cellSize, cellSize * (i + 1), cellSize * 9);
 		}
-		
+
 		// 点
 		g.setColor(Color.black);
 		int pointSize = (int) (cellSize * 0.1);
@@ -93,55 +93,54 @@ public class Reversi extends JPanel {
 	/**
 	 * 初期駒描画
 	 */
-    private void pieceDraw(Graphics g) {
+	private void pieceDraw(Graphics g) {
 		for (int iy = 0; iy < board.length; iy++) {
 			for (int jx = 0; jx < board[iy].length; jx++) {
-				if(board[iy][jx] == "○") {
+				if (board[iy][jx] == "○") {
 					// 白丸
-					putWhitePiece(jx +"," + iy , g);
-				} else if(board[iy][jx] == "●") {
+					putWhitePiece(jx + "," + iy, g);
+				} else if (board[iy][jx] == "●") {
 					// 黒丸
-					putBlackPiece(jx +"," + iy , g);
+					putBlackPiece(jx + "," + iy, g);
 				}
 			}
 		}
-    }
-    
+	}
+
 	/**
 	 * 駒配置
 	 */
-    private void putPiece(int x, int y, Color c, Graphics g){
+	private void putPiece(int x, int y, Color c, Graphics g) {
 		g.setColor(c);
-        g.fillOval(x + masuyohaku, y + masuyohaku, cellSize - masuyohaku * 2, cellSize - masuyohaku * 2);
-    }
-
+		g.fillOval(x + masuyohaku, y + masuyohaku, cellSize - masuyohaku * 2, cellSize - masuyohaku * 2);
+	}
 
 	/**
 	 * 白駒配置
 	 */
-    private void putWhitePiece(String cellAddress, Graphics g){
-    	String[] address = cellAddress.split(",");
-    	int x = Integer.parseInt(address[0]);
-    	int y = Integer.parseInt(address[1]);
+	private void putWhitePiece(String cellAddress, Graphics g) {
+		String[] address = cellAddress.split(",");
+		int x = Integer.parseInt(address[0]);
+		int y = Integer.parseInt(address[1]);
 		putPiece(cellSize * x, cellSize * y, Color.white, g);
-    }
-    
+	}
+
 	/**
 	 * 黒駒配置
-	 */    
-    private void putBlackPiece(String cellAddress, Graphics g){
-    	String[] address = cellAddress.split(",");
-    	int x = Integer.parseInt(address[0]);
-    	int y = Integer.parseInt(address[1]);
+	 */
+	private void putBlackPiece(String cellAddress, Graphics g) {
+		String[] address = cellAddress.split(",");
+		int x = Integer.parseInt(address[0]);
+		int y = Integer.parseInt(address[1]);
 		putPiece(cellSize * x, cellSize * y, Color.black, g);
-    }
-    
-    // クリックされた時の処理用のクラス
-    class MouseProc extends MouseAdapter {
-        public void mouseClicked(MouseEvent e) {
-            int x = e.getX();
-            int y = e.getY();
-            // 盤の外側がクリックされたときは何もしないで終了
+	}
+
+	// クリックされた時の処理用のクラス
+	class MouseProc extends MouseAdapter {
+		public void mouseClicked(MouseEvent e) {
+			int x = e.getX();
+			int y = e.getY();
+			// 盤の外側がクリックされたときは何もしないで終了
 			if (x < leftBlank) {
 				return;
 			} else if (x >= leftBlank + cellSize * 8) {
@@ -164,33 +163,33 @@ public class Reversi extends JPanel {
 		}
 	}
 
-    public int countFlip(int row, int col, boolean bwturn, int dr, int dc) {
-        int count = 0;
-        int r = row + dr;
-        int c = col + dc;
-        // 盤からはみ出さない間繰り返す
-        while (0 <= r && r < board[0].length && 0 <= c && c < board.length) {
-            if (board[c][r] == puttablePiece(!bwTurn)){
-                // 相手の石なら countを1増やす
-                count++;
-            } else if (board[c][r] == puttablePiece(bwTurn)) {
-                // 自分の石なら count を返して終了
-                return count;
-            } else {
-                // 空マスなら 0 を返して終了
-                return 0;
-            }
-            // 次のマスへ移動
-            r += dr;
-            c += dc;
-        }
-        // 盤からはみ出したら0を返す
-        return 0;
-    }
-    
+	public int countFlip(int row, int col, boolean bwturn, int dr, int dc) {
+		int count = 0;
+		int r = row + dr;
+		int c = col + dc;
+		// 盤からはみ出さない間繰り返す
+		while (0 <= r && r < board[0].length && 0 <= c && c < board.length) {
+			if (board[c][r] == puttablePiece(!bwTurn)) {
+				// 相手の石なら countを1増やす
+				count++;
+			} else if (board[c][r] == puttablePiece(bwTurn)) {
+				// 自分の石なら count を返して終了
+				return count;
+			} else {
+				// 空マスなら 0 を返して終了
+				return 0;
+			}
+			// 次のマスへ移動
+			r += dr;
+			c += dc;
+		}
+		// 盤からはみ出したら0を返す
+		return 0;
+	}
+
 	private boolean canPut(int row, int col, boolean bwTurn) {
 		boolean result = false;
-        // その場所が空でなかった置けない
+		// その場所が空でなかった置けない
 		if (board[col][row] == "・") {
 			// どこかの方向でひっくりかせれば OK
 			for (int i = 0; i < 8; i++) {
@@ -204,28 +203,28 @@ public class Reversi extends JPanel {
 
 	private void put(int row, int col, boolean bwTurn) {
 		board[col][row] = puttablePiece(bwTurn);
-        // 各方向について，ひっくり返せる石をひっくり返す
-        for (int i = 0; i < 8; i++) {
-            int count = countFlip(row, col, bwTurn, dr[i], dc[i]);
-            if (count > 0){
-                flip(row, col, dr[i], dc[i], count);
-            }
-        }
+		// 各方向について，ひっくり返せる石をひっくり返す
+		for (int i = 0; i < 8; i++) {
+			int count = countFlip(row, col, bwTurn, dr[i], dc[i]);
+			if (count > 0) {
+				flip(row, col, dr[i], dc[i], count);
+			}
+		}
 	}
-	
-    public void flip(int row, int col, int dr, int dc, int count) {
-        int r = row + dr;
-        int c = col + dc;
-        for (int i = 0; i < count; i++) {
-        	board[c][r] = opponent(board[c][r]);
-            r += dr;
-            c += dc;
-        }
-    }
-    
-    private String opponent(String piece) {
-    	return piece=="●" ? "○" : "●";
-    }
+
+	public void flip(int row, int col, int dr, int dc, int count) {
+		int r = row + dr;
+		int c = col + dc;
+		for (int i = 0; i < count; i++) {
+			board[c][r] = opponent(board[c][r]);
+			r += dr;
+			c += dc;
+		}
+	}
+
+	private String opponent(String piece) {
+		return piece == "●" ? "○" : "●";
+	}
 
 	private String puttablePiece(boolean bwTurn) {
 		if (bwTurn) {
@@ -236,17 +235,17 @@ public class Reversi extends JPanel {
 			return "●";
 		}
 	}
-    
+
 	/**
 	 * 起動
 	 */
-    public static void main(String[] args) {
-        JFrame f = new JFrame();
-        f.getContentPane().setLayout(new FlowLayout());
-        f.getContentPane().add(new Reversi());
-        f.pack();
-        f.setResizable(false);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
-    }
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.getContentPane().setLayout(new FlowLayout());
+		f.getContentPane().add(new Reversi());
+		f.pack();
+		f.setResizable(false);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
 }
